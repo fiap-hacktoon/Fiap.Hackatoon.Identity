c2b6e68075a89e018bf49f0314c1855aa1335d17
rodrigo mahlow <rmahlow@gmail.com>
2025-06-17 21:46:00 -0300

    add kubernets files

---
+++ Fiap.Hackatoon.Identity.API/Kubernets/deploy.sh
@@ -0,0 +1,21 @@
+set -e  # Encerra o script em caso de erro
+
+echo "🚪 Aplicando Rabbitmq Deployment.."
+kubectl apply -f RabbitmqDeployment.yaml  # Aplica o Service da aplicação
+
+echo "🚪 Aplicando Rabbitmq Service..."
+kubectl apply -f RabbitmqService.yaml  # Aplica o Service da aplicação
+
+echo "🔐 Aplicando Secret da aplicação..."
+kubectl apply -f app-secrets.yaml  # Adiciona o Secret
+
+echo "⚙️ Aplicando ConfigMap da aplicação..."
+kubectl apply -f Configmap.yaml  # Aplica o ConfigMap
+
+echo "📦 Aplicando Deployment da aplicação..."
+kubectl apply -f Deployment.yaml  # Aplica o Deployment da aplicação
+
+echo "🚪 Aplicando Service da aplicação..."
+kubectl apply -f Service.yaml  # Aplica o Service da aplicação
+
+echo "✅ Tudo aplicado com sucesso!"
